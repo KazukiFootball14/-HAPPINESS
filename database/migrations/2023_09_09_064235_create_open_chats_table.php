00d4d3b4753899f8d7e77aacefff5533798fe6be
KazukiFootball14 <kazuki.football14@gmail.com>
2023-09-09 17:23:48 +0900

    マイグレーション実行によるテーブル作成とカラム追加

---
+++ database/migrations/2023_09_09_064235_create_open_chats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('open_chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained();
+            $table->foreignId("recruitment_id")->constrained();
+            $table->foreignId("post_id")->constrained();
+            $table->timestamps("created_at");
+            $table->timestamps("updated_at");
+            $table->string("message");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('open_chats');
+    }
+};
