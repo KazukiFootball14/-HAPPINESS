9d9b8cd95f61d624cc141b061ec9b94b130182b0
KazukiFootball14 <kazuki.football14@gmail.com>
2023-09-10 18:55:20 +0900

    Viewにデータ埋め込み

---
+++ resources/views/recruitments/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <div class='recruitments'>
+            @foreach ($recruitments as $recruitment)
+                <div class='recruitment'>
+                    <h2 class='title'>{{ $recruitment->title }}</h2>
+                    <p class='body'>{{ $recruitment->body }}</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
+</html>
